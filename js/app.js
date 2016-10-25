f6a5383a327ab93e3bc74760e1fc005f4e0f171c
abhishek b. jha <abhishek b. jha>
2016-10-26 01:34:13 +0530

    This is knockout framework based cat-clicker

--- js/app.js
+++ js/app.js
@@ -2,12 +2,8 @@ var model=function(){
     this.name=ko.observable("pussy_cat");
     this.imgsrc=ko.observable("img/a.jpg");
     this.clickCount=ko.observable(0);
-    // this.st=ko.observableArray(["borned","infant"])
     this.status=ko.observable("newly_borned");
-
-   
-   // this.names=ko.observable(["img/a.jpg","img/b.jpg","img/c.jpg","img/d.jpg","img/e.jpg"]);
-}
+    }
 this.names=ko.observableArray([{values:"ashish"},
             {values:"ddd"}]);
 
