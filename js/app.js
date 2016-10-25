9e4c05018e608b5b0223c0365de730119cb45995
abhishek b. jha <abhishek b. jha>
2016-10-26 01:23:22 +0530

    all fuctionality of cat is merged into single function

--- js/app.js
+++ js/app.js
@@ -1,37 +1,37 @@
 var model=function(){
-    this.name=ko.observable("pussy_cat");
-    this.imgsrc=ko.observable("img/a.jpg");
-    this.clickCount=ko.observable(0);
-    // this.st=ko.observableArray(["borned","infant"])
-    this.status=ko.observable("newly_borned");
-
+ 
+ this.currentCat=ko.observable(new Cat());
    
-   // this.names=ko.observable(["img/a.jpg","img/b.jpg","img/c.jpg","img/d.jpg","img/e.jpg"]);
+  
 }
-this.names=ko.observableArray([{values:"ashish"},
+
+var Cat=function(){
+
+	this.name=ko.observable("pussy_cat");
+	this.imgsrc=ko.observable("img/a.jpg");
+	this.clickCount=ko.observable(0);
+	this.status=ko.observable("newly_borned");
+	this.names=ko.observableArray([{values:"ashish"},
             {values:"ddd"}]);
-
-
-
-
-this.change_status=function(){
-   this.status=ko.observable("bas");
-
 }
+
+
+
+
 
 this.incrementCounter=function(){
 
-	 this.clickCount(this.clickCount()+1);
-	 if (this.clickCount()>5){
-	 	this.status("Now became infant");
+	 this.currentCat().clickCount(this.currentCat().clickCount()+1);
+	 if (this.currentCat().clickCount()>5){
+	 	this.currentCat().status("Now became infant");
 	 }
 
-	  if (this.clickCount()>10){
-	 	this.status("Now became Child");
+	  if (this.currentCat().clickCount()>10){
+	 	this.currentCat().status("Now became Child");
 	 }
 
-	  if (this.clickCount()>15){
-	 	this.status("Now became Adult");
+	  if (this.currentCat().clickCount()>15){
+	 	this.currentCat().status("Now became Adult");
 	 }
 	 
     
